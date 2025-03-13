815c43fadb4f5c5a805da9e354920b6d626ccd09
Mauricio Silva <mrcvr1@gmail.com>
2025-03-13 11:20:10 -0300

    feat: creating fetch all players route

--- src/jogadores/jogadores.controller.ts
+++ src/jogadores/jogadores.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CriarJogadorDto } from './dtos/criar-jogador.dto';
 import { JogadoresService } from './jogadores.service';
+import { Jogador } from './interfaces/jogador.interface';
 
 @Controller('api/v1/jogadores')
 export class JogadoresController {
@@ -12,5 +13,10 @@ export class JogadoresController {
         @Body() criarJogadorDto: CriarJogadorDto
     ) {
         await this.jogadoresService.criaAtualizarJogador(criarJogadorDto);
+    }
+
+    @Get()
+    async consultarJogadores(): Promise<Jogador[]> {
+        return this.jogadoresService.consultarTodosJogadores();
     }
 }
